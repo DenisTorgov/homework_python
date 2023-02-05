0c52bc0269991a46ba445533a9620cf644aa9045
Denis.Torgov <deniska-3000@bk.ru>
2023-02-05 08:33:32 +0300

    task_6_1 complited

---
+++ seminar_6/task_6_1.py
@@ -0,0 +1,14 @@
+# Напишите программу, удаляющую из текста все слова,
+# содержащие ""абв"".
+
+stringA = "абв пшзяабв в рпыщзь невчннг опабвпм авыолб гач бва пвавас арогбсфв"
+
+stringB = " ".join(filter(lambda i: i.find("абв") < 0, stringA.split()))
+
+# stringB = ""
+# for i in stringA.split():
+#     print(i)
+#     if i.find("абв") < 0:
+#         stringB = stringB + "".join(i) + " "
+
+print(stringB)
